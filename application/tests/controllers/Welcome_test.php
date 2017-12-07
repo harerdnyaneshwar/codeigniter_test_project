d7215b0a426c9b969f4d40af74a20030116fd090
Dnyaneshwar Harer <dnyaneshwar.h@sankeysolutions.com>
2017-12-07 12:42:57 +0530

    corrected change

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -13,7 +13,7 @@ class Welcome_test extends TestCase
 	public function test_index()
 	{
 		$output = $this->request('GET', 'welcome/index');
-		$this->assertContains('<title>Welcome to CodeIgniterHello</title>', $output);
+		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
 	}
 
 	public function test_method_404()
